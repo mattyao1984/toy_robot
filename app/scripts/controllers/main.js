7d2b4ac624fd4618b1154f3525ff5fc2a9089fb9
Matt Yao <matt@camq.com.au>
2015-02-21 09:41:47 +1100

    Init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name freelanceWorkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the freelanceWorkApp
+ */
+angular.module('freelanceWorkApp')
+  .controller('MainCtrl', function ($scope) {
+  	
+  });
